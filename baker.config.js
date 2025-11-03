ab607bf6aeeb0a4f95e266e1b6be58ebaf1a90c5
dmarchese76 <d.marchese76@journalism.cuny.edu>
2025-11-03 17:56:17 -0500

    add files

--- baker.config.js
+++ baker.config.js
@@ -10,8 +10,6 @@ export default {
   entrypoints: `scripts/${
     entrypoints.length > 1 ? `{${entrypoints.join(',')}}` : entrypoints[0]
   }.js`,
-  pathPrefix:
-    process.env.BAKER_PATH_PREFIX || process.env.DELIVERY_BASE_PATH || '/',
   // An example of how creating dynamic pages, as described in the README
   // createPages(createPage, data) {
   //   const pageList = data.example;
